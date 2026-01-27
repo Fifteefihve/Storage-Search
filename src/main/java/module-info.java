680a41e449736f43e5c67fda06418a09672bf0cb
Fifteefihve <121834938+Fifteefihve@users.noreply.github.com>
2026-01-27 14:29:26 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.storagesearch {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.storagesearch to javafx.fxml;
-    exports org.example.storagesearch;
-}
